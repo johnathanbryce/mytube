f6f9e77bd5fc991d3c16050a04aa1dca27f858e0
johnathanbryce <johnathanbryce@rompathlete.com>
2023-06-15 14:29:45 -0700

    got toggle working, need to work on styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,12 @@
 import styles from './Header.module.css'
+// internal components
+import ThemeToggle from '../ThemeToggle/ThemeToggle'
 
 function Header() {
   return (
     <header className={styles.header}>
         <h1> MediaBlend</h1>
+        <ThemeToggle />
     </header>
   )
 }
