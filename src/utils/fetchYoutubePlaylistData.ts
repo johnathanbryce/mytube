ab5483085ad1e7c53c8d9f064bcd13767e3d0b53
johnathanbryce <johnathanbryce@rompathlete.com>
2023-06-22 10:42:27 -0700

    a tonne of changes, biggest one is moving the API call to the page.tsx parent component to reduce api calls every time the containers were opened and closed

--- src/utils/fetchYoutubePlaylistData.ts
+++ src/utils/fetchYoutubePlaylistData.ts
@@ -1,39 +1,32 @@
 const apiKey = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY;
 
 interface fetchYoutubePlaylistDataProps {
-  urls: any;
+  urls: string[];
 }
 
 async function fetchYoutubePlaylistData({ urls }: fetchYoutubePlaylistDataProps) {
     try {
       const dataPromises = urls.map((url: any) => fetchPlaylistData(url));
       const data = await Promise.all(dataPromises);
-
-      return data;
+      return { data, error: false};
     } catch (err) {
-      throw new Error('Failed to fetch YouTube playlist data');
+      return { data: [], error: true};                    // change to false if testing for error messages in dev
     }
   }
   
   async function fetchPlaylistData(url: string) {
     try {
       const res = await fetch(`${url}&key=${apiKey}`);
+      console.log('data fetched')
       if (!res.ok) {
-        throw new Error('Failed to fetch data from the YouTube API');
+        throw new Error('Failed to fetch data');         // triggers catch statement below
       }
       const data = await res.json();
       return data.items;
     } catch (err) {
-      console.error(err);
-      return [];
+      throw new Error('Error fetching data')             // triggers catch statement above
+      //return [];
     }
   }
-
-
-
-  
-  
-
-  
 
 export default fetchYoutubePlaylistData;
